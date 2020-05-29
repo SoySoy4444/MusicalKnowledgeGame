522aafdf1331101015bc5a54691662cb66540917
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2020-05-29 19:44:14 +0900

    Add User class

--- game.py
+++ game.py
@@ -1,40 +1,56 @@
 import os
 import json
 import spotipy
-import sys
 import spotipy.util as util
 from spotipy import SpotifyException
 from dotenv import load_dotenv
 import random
 
-# username = sys.argv[0]
-username = "SoySoy4444"
 load_dotenv()
 CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
+
+class User:
+    def __init__(self, username, scope):
+        try:
+            self.token = util.prompt_for_user_token(
+                username,
+                scope=scope,
+                client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                redirect_uri=REDIRECT_URI
+            )
+        except SpotifyException:
+            os.remove(f".cache-{username}")
+            self.token = util.prompt_for_user_token(
+                username,
+                scope=scopes,
+                client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                redirect_uri=REDIRECT_URI
+            )
+
+        self.spotify_obj = spotipy.Spotify(auth=self.token)
+        self.user = self.spotify_obj.current_user()
+
+        self.display_name = self.user["display_name"]
+        self.followers = self.user["followers"]["total"]
+        self.uri = self.user["uri"]
+
+        # https://developer.spotify.com/documentation/web-api/reference/playlists/get-a-list-of-current-users-playlists/
+        self.playlists = self.spotify_obj.user_playlists(username)
+
+
 # https://developer.spotify.com/documentation/general/guides/scopes/
 scopes = "user-follow-read user-library-read playlist-read-private"
+user = User("SoySoy4444", scopes)
+user_playlists = user.playlists
+playlists_names_list = [playlist["name"] for playlist in user_playlists["items"]]
 
-try:
-    token = util.prompt_for_user_token(
-        username,
-        scope=scopes,
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI
-    )
-except SpotifyException:
-    os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(
-        username,
-        scope=scopes,
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI
-    )
-
-spotify_obj = spotipy.Spotify(auth=token)
-user = spotify_obj.current_user()
-print(json.dumps(user, sort_keys=True, indent=4))
+print(f"Welcome, {user.display_name}!")
+print(f"You have {user.followers} followers.")
+print("You have the following playlists: ")
+print(playlists_names_list)
+print()
