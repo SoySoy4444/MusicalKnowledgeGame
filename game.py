d03eb139e0cb7eaf3fbf71d185beb6ce45eb6809
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2020-05-31 10:15:40 +0900

    Change to multiple choice quiz, add !next command

--- game.py
+++ game.py
@@ -12,6 +12,13 @@ CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+discord_client = discord.Client()
+
+oauth = spotipy.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+token = oauth.get_access_token(as_dict=False)
+spotipy_client = spotipy.client.Spotify(auth=token)
+
+current_playlist, correct_answer, mode, game_playlists_iterator = None, None, None, None
 
 
 class User:
@@ -58,89 +65,130 @@ class Playlist:
 
 class Song:
     def __init__(self, playlist):  # accepts a Playlist object
+        self.playlist = playlist
         self.num_songs = len(playlist.tracks["items"])
         self.index = random.randint(0, self.num_songs-1)
         self.song = playlist.tracks["items"][self.index]
-        print("SELF SONG")
-        print(json.dumps(self.song, indent=4))
 
-        self.artist_names = [artist["name"] for artist in self.song["track"]["artists"]] # there may be multiple artists
+        self.artist_names = [artist["name"] for artist in self.song["track"]["artists"]]  # possibly multiple artists
         self.album = self.song["track"]["album"]["name"]
         self.name = self.song["track"]["name"]
 
+        self.used_songs_indices = [self.index]  # stores the indices of songs used for the correct answer and decoys
+
     def get_random_question(self):
         questions = {
-            0: (f"Who is the artist of {self.name}?", self.artist_names),
+            0: (f"Who is the artist of {self.name}?", " & ".join(self.artist_names)),
             1: (f"What is the album name of {self.name}?", self.album),
         }
+        question_type = random.randint(0, len(questions) - 1)
+        random_question = questions[question_type][0]
+        correct_answer_ = questions[question_type][1]
 
-        question_number = random.randint(0, len(questions) - 1)
-        random_question = questions[question_number][0]
-        answer = questions[question_number][1]
-        return random_question, answer
+        choices = ["A) ", "B) ", "C) ", "D) "]
+
+        answers_list = []  # multiple choice answers
+        correct_answer_index = random.randint(0, 3)  # the index of the correct answer, 0 - 3.
+        for i in range(len(choices)):  # for each answer choice
+            if i == correct_answer_index:  # for the correct answer,
+                answers_list.append(choices[i] + correct_answer_)
+                continue
+
+            while True:
+                if (random_song_index := random.randint(0, self.num_songs-1)) not in self.used_songs_indices:
+                    self.used_songs_indices.append(random_song_index)
+                    break
+            random_song = self.playlist.tracks["items"][random_song_index]
+            if question_type == 0:
+                random_song_artists = [artist["name"] for artist in random_song["track"]["artists"]]
+                answers_list.append(choices[i] + " & ".join(random_song_artists))
+            elif question_type == 1:
+                answers_list.append(choices[i] + random_song["track"]["album"]["name"])
+
+        return random_question, answers_list, correct_answer_index
 
 
-# Generate random playlist from a playlist dictionary
-def get_random_playlist(playlist_dict):
-    num_playlists = len(playlist_dict["items"])
-    index = random.randint(0, num_playlists-1)
-    random_playlist = playlist_dict["items"][index]
-    return random_playlist
+async def next_question(playlist, channel):  # accepts a Playlist object
+    song = playlist.get_random_song()
+    global correct_answer
+    question, answer_choices, correct_index = song.get_random_question()
 
-
-discord_client = discord.Client()
-answer = None
-
-
-async def next_question(current_playlist, channel):  # accepts a Playlist object
-    song = current_playlist.get_random_song()
-    global answer
-    question, answer = song.get_random_question()
-
-    await channel.send(question)
+    correct_answer = answer_choices[correct_index]  # set the correct answer to a variable
+    await channel.send(question)  # send question
+    await channel.send("\n".join([answer_choice for answer_choice in answer_choices]))  # send answer choices
 
 
 @discord_client.event
 async def on_message(message):
+    global current_playlist, mode, game_playlists_iterator
     if message.author == discord_client.user:
         return
 
-    if message.content.startswith("!user"):
-        usernames = message.content.split(" ")[1:]
-        await message.channel.send(f"Starting game with playlists by {usernames}")
+    elif message.content.startswith("!user"):
+        mode = "user"
+        username = message.content.split(" ")[0]
+        await message.channel.send(f"Starting game with playlists by {username}")
 
         # https://developer.spotify.com/documentation/general/guides/scopes/
         scopes = "user-library-read"
         try:
-            user = User("SoySoy4444", scopes)  # TODO: Change to username
+            user = User(username, scopes)
 
             user_playlists = user.playlists
+            global spotipy_client
+            spotipy_client = user.spotify_obj  # before, client was only Spotify API, but now we need user access.
 
             playlists_names_list = [playlist["name"] for playlist in user_playlists["items"]]
             print(f"Hello {user.display_name}, you have the following playlists: {playlists_names_list}")
-            current_playlist = Playlist(get_random_playlist(user_playlists), user.spotify_obj)
-            print(f"We will start with... {current_playlist.name}!")
+
+            game_playlists = user_playlists["items"]
+            random.shuffle(game_playlists)
+            game_playlists_iterator = iter(game_playlists)
+            current_playlist = Playlist(next(game_playlists_iterator), spotipy_client)
+            await message.channel.send(f"We will start with... {current_playlist.name}!")
 
             await next_question(current_playlist, message.channel)
-        except SpotifyException:
+        except SpotifyException:  # Authentication failed.
             await message.channel.send("Not a valid username!")
 
-    if message.content.startswith("!featured"):
-        oath = spotipy.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-        token = oath.get_access_token(as_dict=False)
-        client = spotipy.client.Spotify(auth=token)
-
-        featured_playlists = client.featured_playlists()["playlists"]
-        featured_playlists_names = [s["name"] for s in featured_playlists["items"]]
+    elif message.content.startswith("!featured"):
+        mode = "featured"
+        featured_playlists = spotipy_client.featured_playlists()["playlists"]
+        featured_playlists_names = [playlist["name"] for playlist in featured_playlists["items"]]
         await message.channel.send(f"Today's featured playlists are: {featured_playlists_names}")
 
-        current_playlist = Playlist(get_random_playlist(featured_playlists), client)
-        print(f"We will start with... {current_playlist.name}!")
+        game_playlists = featured_playlists["items"]
+        random.shuffle(game_playlists)
+        game_playlists_iterator = iter(game_playlists)
+        current_playlist = Playlist(next(game_playlists_iterator), spotipy_client)
+        await message.channel.send(f"We will start with... {current_playlist.name}!")
 
         await next_question(current_playlist, message.channel)
 
-    if message.content.startswith("!playlist"):
+    elif message.content.startswith("!playlist"):
+        mode = "playlist"
         playlist_uri = message.content.split(" ")[1]
+        current_playlist = Playlist(spotipy_client.playlist(playlist_uri), spotipy_client)
+        await message.channel.send(f"Starting game on {current_playlist.name}!")
+        await next_question(current_playlist, message.channel)
+
+    elif message.content.startswith("!next"):  # move on to the next playlist
+        if mode is None:
+            await message.channel.send("Initialise the game with !user <username>, !featured or !playlist <uri> first!")
+        elif mode == "playlist":
+            await message.channel.send("You cannot move on to the next playlist on playlist mode!")
+        else:
+            try:
+                current_playlist = Playlist(next(game_playlists_iterator), spotipy_client)
+
+                await message.channel.send(f"Next playlist coming up! Now playing... {current_playlist.name}!")
+                await next_question(current_playlist, message.channel)
+            except StopIteration:
+                await message.channel.send("You have played all playlists!")
+
+    elif message.content in correct_answer and ")" in message.content:
+        await message.channel.send(f"Correct, it's {correct_answer}!")
+        await next_question(current_playlist, message.channel)
 
 
 discord_client.run(DISCORD_TOKEN)
@@ -148,5 +196,4 @@ discord_client.run(DISCORD_TOKEN)
 
 # TODO: Add !skip command
 # TODO: Add !hint command
-# TODO: Check answers
 # TODO: Add scoring system
